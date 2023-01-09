98aa4d9330cec9d7f1617874c1e3356644e58a91
Siris Chalise <chalise.krishna19@gmail.com>
2023-01-09 15:25:04 +0530

    changing the text

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	return res.send("working");
+	return res.send("system is in maintenance");
 });
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`server is listining in ${port}`));
